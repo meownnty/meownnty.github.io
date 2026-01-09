534629d026a37ecd4d0dc552f81baf38e032af7b
Abigail Hope Sykes <abigailsykes50@gmail.com>
2026-01-09 17:09:42 +0200

    Attemot #12 please jusr work

--- images/script.js
+++ images/script.js
@@ -3,6 +3,6 @@ const background = document.querySelector(".background-water");
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
 
-    // Move background slower than content
+    // Parallax effect: slower than scrolling
     background.style.backgroundPosition = `center ${scrollY * 0.3}px`;
 });
