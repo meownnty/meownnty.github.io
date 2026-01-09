9ed1e18185a61def1029aace78460ee2060ab50f
Abigail Hope Sykes <abigailsykes50@gmail.com>
2026-01-09 18:46:00 +0200

    attempt #13? i am tired of this please work bacckground

--- images/script.js
+++ images/script.js
@@ -3,6 +3,6 @@ const background = document.querySelector(".background-water");
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
 
-    // Parallax effect: slower than scrolling
-    background.style.backgroundPosition = `center ${scrollY * 0.3}px`;
+    // Background moves slightly slower than scroll
+    background.style.transform = `translateY(${scrollY * 0.3}px)`;
 });
