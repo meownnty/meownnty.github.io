fe9aba871343712f043a9b54cc024a6b6d40ba67
Abigail Hope Sykes <abigailsykes50@gmail.com>
2026-01-09 19:09:28 +0200

    Attempt #15??? Background still not moving please work now

--- images/script.js
+++ images/script.js
@@ -2,6 +2,5 @@ const background = document.querySelector(".background-water");
 
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
-    // Parallax effect: move slightly slower than scroll
     background.style.transform = `translateY(${scrollY * 0.3}px)`;
 });
