e645cb451aa55c9875e75a9ea03fc4c5271c58b4
Abigail Hope Sykes <abigailsykes50@gmail.com>
2026-01-09 16:37:43 +0200

    Attempt #7? background fix

--- images/script.js
+++ images/script.js
@@ -1,9 +1,6 @@
-const background = document.querySelector(".background-water");
-const img = new Image();
-img.src = "images/background.jpeg";
-img.onload = () => {
-    const imageHeight = img.height;
-    // Set div height to match image
-    background.style.height = `${imageHeight}px`;
-};
-
+window.addEventListener("scroll", () => {
+    document.documentElement.style.setProperty(
+        "--scroll",
+        window.scrollY + "px"
+    );
+});
