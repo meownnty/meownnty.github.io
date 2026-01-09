d53db572ee0cf822c109bef44967f9f84eb4d685
Abigail Hope Sykes <abigailsykes50@gmail.com>
2026-01-09 19:17:40 +0200

    Attempt #16

--- images/script.js
+++ images/script.js
@@ -2,5 +2,6 @@ const background = document.querySelector(".background-water");
 
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
+    // parallax: move background slower than content
     background.style.transform = `translateY(${scrollY * 0.3}px)`;
 });
