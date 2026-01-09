de49966aed1a703f1dedbe6148c505b28786c327
Abigail Hope Sykes <abigailsykes50@gmail.com>
2026-01-09 16:59:19 +0200

    Attempt #10 to make background work

--- images/script.js
+++ images/script.js
@@ -3,6 +3,6 @@ const background = document.querySelector(".background-water");
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
 
-    // Move background slower than content for parallax
-    background.style.transform = `translateY(${scrollY * 0.3}px)`;
+    // Move background slower than content
+    background.style.backgroundPosition = `center ${scrollY * 0.3}px`;
 });
