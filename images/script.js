9656a179ad09b200b488f44f9756d26fc0007b25
Abigail Hope Sykes <abigailsykes50@gmail.com>
2026-01-09 14:00:19 +0200

    Attempt i lost count to fix background movement

--- images/script.js
+++ images/script.js
@@ -1,11 +1,7 @@
 // Grab the background div
 const background = document.querySelector(".background-water");
 
-// Listen for scrolling
 window.addEventListener("scroll", () => {
-    // Get how much the page has scrolled
     const scrollY = window.scrollY;
-
-    // Move the background slower than scroll for parallax effect
-    background.style.transform = `translateY(${scrollY * 0.3}px)`;
+    background.style.top = `${-scrollY * 0.3}px`; // move it up as we scroll
 });
