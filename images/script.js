622eb3c7b2eebf398f39c068d9d86d93d8b13243
Abigail Hope Sykes <abigailsykes50@gmail.com>
2026-01-09 14:11:14 +0200

    Adjustments to make background fit

--- images/script.js
+++ images/script.js
@@ -1,8 +1,13 @@
 const background = document.querySelector(".background-water");
+const img = new Image();
+img.src = "images/background.jpeg";
+img.onload = () => {
+    const imageHeight = img.height;
+    // Set div height to match image
+    background.style.height = `${imageHeight}px`;
+};
 
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
-
-    // move background down slightly slower than scroll
     background.style.backgroundPosition = `center ${scrollY * 0.5}px`;
 });
