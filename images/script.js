92bdde3062eeb32eee14edebc5726e93ba5f5acf
Abigail Hope Sykes <abigailsykes50@gmail.com>
2026-01-09 19:02:58 +0200

    I lost count again another attempt to get this background to move

--- images/script.js
+++ images/script.js
@@ -2,6 +2,6 @@ const background = document.querySelector(".background-water");
 
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
-    // Move background slightly slower than content for parallax
+    // Parallax effect: move slightly slower than scroll
     background.style.transform = `translateY(${scrollY * 0.3}px)`;
 });
